bf5998b216018a847c59c9b1d213c7f4fc3955cd
Faisal Adam Yudithia <first.yudithia@gmail.com>
2022-05-04 17:21:50 +0700

    Cleansing

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const cells = document.querySelectorAll(".cell span");
+const keyBtns = document.querySelectorAll("#key button");
+
+cells.forEach((c) => {
+	c.addEventListener("click", () => {
+		c.id = "selected";
+		let activeKey = document.querySelector("#key button.active");
+		if (activeKey != null) {
+			c.textContent = activeKey.textContent[0];
+			c.classList.add("active");
+		} else {
+			[...cells]
+				.filter((cell) => cell.textContent == c.textContent)
+				.forEach((fc) => {
+					fc.classList.toggle("active");
+				});
+			[...cells]
+				.filter((cell) => cell.textContent != c.textContent)
+				.forEach((fc) => {
+					fc.classList.remove("active");
+				});
+		}
+	});
+});
+
+keyBtns.forEach((key) => {
+	key.addEventListener("click", () => {
+		let selectedCell = document.querySelector(".cell span#selected");
+		if (selectedCell != null) {
+			selectedCell.textContent = key.textContent[0];
+		}
+		key.classList.toggle("active");
+		[...keyBtns]
+			.filter((keyBtn) => keyBtn != key)
+			.forEach((k) => {
+				k.classList.remove("active");
+			});
+		[...cells]
+			.filter((cell) => cell.textContent == key.textContent[0])
+			.forEach((fc) => {
+				fc.classList.toggle("active");
+			});
+		[...cells]
+			.filter((cell) => cell.textContent != key.textContent[0])
+			.forEach((fc) => {
+				fc.classList.remove("active");
+			});
+	});
+});
+
+myArray = [1, 2, 3, 4, 5, 6, 8, 10];
+function partition(array, isValid) {
+	return array.reduce(
+		([active, inactive], elem) => {
+			return isValid(elem)
+				? [[...active, elem], inactive]
+				: [active, [...inactive, elem]];
+		},
+		[[], []]
+	);
+}
+// console.log([1, 100]);
+const x = partition(myArray, (e) => e < 5);
---
+++ js/sudoku.js
@@ -0,0 +1,129 @@
+/* Generate sudoku */
+let sudoku = initSudoku();
+let iterCount = 1;
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+generateSudoku(sudoku);
+
+/* ================================================== */
+
+/* Function to init blank sudoku */
+function initSudoku() {
+  let sudoku = [];
+  for (let i = 0; i < 9; i++)
+    sudoku.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  return sudoku;
+};
+
+/* Function to generate sudoku */
+function generateSudoku(sudoku) {
+  // printIter();
+  let x; // Row
+  let y; // Column
+  for (let i = 0; i < 81; i++) {
+    x = Math.floor(i / 9);
+    y = i % 9;
+    if (sudoku[x][y] == 0) {
+      shuffleArr(num);
+      for (n in num) {
+        let value = num[n];
+        // Check if the value has not already used in this row
+        if (sudoku[x].includes(value) == false) {
+          let arrNumUsedColumn = numUsedColumn(sudoku, y);
+          // Check if the value has not already used in this column
+          if (arrNumUsedColumn.includes(value) == false) {
+            let arrNumUsedBlock = numUsedBlock(sudoku, x, y);
+            // Check if the value has not already used in this block
+            if (arrNumUsedBlock.includes(value) == false) {
+              sudoku[x][y] = value;
+              if (isValidSudoku(sudoku)) {
+                // printSudoku(sudoku);
+                return true
+              } else {
+                if (generateSudoku(sudoku)) {
+                  return true
+                };
+              };
+            };
+          };
+        };
+      };
+      break;
+    };
+  };
+  sudoku[x][y] = 0;
+};
+
+/* ================================================== */
+
+/* Function to shuffle an array */
+function shuffleArr(arr) {
+  let arrId = arr.length;
+  while (arrId != 0) {
+    let randId = Math.floor(Math.random() * arrId);
+    arrId--;
+    let valSwap = arr[arrId];
+    arr[arrId] = arr[randId];
+    arr[randId] = valSwap;
+  };
+};
+/* Function to collect all values that has already used in a column */
+function numUsedColumn(sudoku, col) {
+  let arrNumUsed = [];
+  for (r in sudoku) {
+    if (sudoku[r][col] != 0) {
+      arrNumUsed.push(sudoku[r][col]);
+    };
+  };
+  return arrNumUsed;
+};
+
+/* Function to collect all values that has already used in a block */
+function numUsedBlock(sudoku, row, col) {
+  let arrNumUsed = [];
+  const rMin = Math.floor(row / 3) * 3;
+  const rMax = rMin + 3
+  for (let r = rMin; r < rMax; r++) {
+    const cMin = Math.floor(col / 3) * 3;
+    const cMax = cMin + 3
+    for (let c = cMin; c < cMax; c++) {
+      if (sudoku[r][c] != 0) {
+        arrNumUsed.push(sudoku[r][c]);
+      };
+    };
+  };
+  return arrNumUsed;
+};
+
+/* Function to validate sudoku */
+function isValidSudoku(sudoku) {
+  for (let x = 0; x < sudoku.length; x++) {
+    for (let y = 0; y < sudoku[x].length; y++) {
+      if (sudoku[x][y] == 0) {
+        return false;
+      };
+    };
+  };
+  return true;
+};
+
+/* ================================================== */
+
+/* Function to print generate sudoku iteration */
+function printIter() {
+  console.clear();
+  console.log("Iteration : " + iterCount);
+  iterCount++;
+}
+
+/* Function to print generated sudoku */
+function printSudoku(sudoku) {
+  for (let x = 0; x < sudoku.length; x++) {
+    console.log(
+      sudoku[x][0] + "  " + sudoku[x][1] + "  " + sudoku[x][2] + "  " + "|" + "  " +
+      sudoku[x][3] + "  " + sudoku[x][4] + "  " + sudoku[x][5] + "  " + "|" + "  " +
+      sudoku[x][6] + "  " + sudoku[x][7] + "  " + sudoku[x][8] + "  "
+    );
+    if (x == 2 || x == 5)
+      console.log("— — — — — — — — — — — — — — — —");
+  };
+};
