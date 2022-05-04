f6d9e2450046618343af1379b59774f50865e14f
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-05-04 18:35:41 +0700

    UI Script

--- public/script.js
+++ public/script.js
@@ -1,78 +1,150 @@
 const cells = document.querySelectorAll(".cell span");
 const keyBtns = document.querySelectorAll("#key button");
 
-function activateSameCell(valValid) {
-	cells.forEach((cell) => {
-		cell.classList.remove("active");
-		valValid(cell.textContent)
-			? cell.classList.add("active")
-			: cell.classList.remove("active");
-	});
-}
-function deactivateSameCell(specified) {
-	let sameCell = cells;
-	if (specified != null) sameCell = specified;
-	if (document.querySelector("#key button.active"))
-		document.querySelector("#key button.active").classList.remove("active");
-	sameCell.forEach((cell) => {
-		cell.classList.remove("active");
-	});
-}
+const randInt = (min, max) => {
+	// min and max included
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
+const activate = (elm) => {
+	if (elm.length) {
+		elm.forEach((e) => {
+			e.classList.add("active");
+		});
+	} else {
+		elm.classList.add("active");
+	}
+};
+
+const deactivate = (elm) => {
+	if (elm) {
+		elm.forEach((e) => {
+			e.classList.remove("active");
+		});
+	} else {
+		document.querySelectorAll(".active").forEach((e) => {
+			e.classList.remove("active");
+		});
+	}
+};
+
+const assignValue = (value) => {
+	const selectedCell = document.querySelector(".cell #selected");
+	if (selectedCell && !selectedCell.classList.contains("disable")) {
+		selectedCell.textContent = value;
+		console.log(value);
+		value != ""
+			? activate([...cells].filter((elm) => elm.textContent == value))
+			: "";
+	}
+};
+
+// init cell
+cells.forEach((cell) => {
+	let value = randInt(-9, 9);
+	value > 0
+		? ((cell.textContent = value), cell.classList.add("disable"))
+		: (cell.textContent = "");
+});
+
+// event for cell
 cells.forEach((cell) => {
 	cell.addEventListener("click", () => {
-		if (document.querySelector("#key button.active") != null)
-			cell.textContent =
-				document.querySelector("#key button.active").textContent;
-		if (!cell.classList.contains("disable")) selectedCell = cell;
-		cell.classList.contains("active")
-			? (deactivateSameCell(), (selectedCell = undefined))
-			: cell.textContent.length > 0
-			? activateSameCell((e) => e == cell.textContent)
-			: (deactivateSameCell(
-					[...cells].filter((e) => e.textContent.length == 0)
-			  ),
-			  cell.classList.toggle("active"));
-	});
-});
-
-keyBtns.forEach((keybtn) => {
-	keybtn.addEventListener("click", () => {
-		if (typeof selectedCell != "undefined")
-			selectedCell.textContent = keybtn.textContent;
-		for (let sibling of keybtn.parentNode.parentNode.children) {
-			btn = sibling.querySelector("button");
-			btn === keybtn
-				? btn.classList.toggle("active")
-				: btn.classList.remove("active");
+		try {
+			document.querySelector(".cell span#selected").id = "";
+		} catch (e) {
+			console.error();
+		} finally {
+			cell.id = "selected";
 		}
 
-		!keybtn.classList.contains("active")
-			? deactivateSameCell()
-			: activateSameCell((e) => e == keybtn.textContent);
+		let selectedKey = document.querySelector("#key button#selected");
+		if (!cell.classList.contains("active")) {
+			if (cell.textContent.length > 0) {
+				deactivate();
+				activate(
+					[...cells].filter((elm) => elm.textContent == cell.textContent)
+				);
+			} else {
+				deactivate(
+					[...cells].filter((elm) => elm.textContent == cell.textContent)
+				);
+				activate(cell);
+			}
+
+			if (selectedKey) {
+				// selectedKey.classList.add("active");
+				if (!cell.classList.contains("disable")) {
+					valueToAssign = document.querySelector(
+						"#key button#selected"
+					).textContent;
+					console.log();
+					assignValue(
+						valueToAssign > 0 && valueToAssign < 9 ? valueToAssign : ""
+					);
+				}
+			}
+		} else {
+			console.log(true);
+			deactivate();
+		}
+
+		if (selectedKey && !selectedKey.classList.contains("active"))
+			selectedKey.id = "";
 	});
 });
 
-document.addEventListener("keydown", (key) => {
-	// check key is number and higher than 0
-	typeof selectedCell !== "undefined"
-		? /^\d+$/.test(key.key) && key.key > 0
-			? ((selectedCell.textContent = key.key),
-			  activateSameCell((e) => e == key.key))
-			: key.keyCode == 8 || key.keyCode == 46
-			? (selectedCell.textContent = "")
-			: ""
-		: "";
+// event for key
+keyBtns.forEach((keyBtn) => {
+	keyBtn.addEventListener("click", () => {
+		let selectedCell = document.querySelector(".cell span#selected");
+		if (selectedCell) {
+			assignValue(
+				keyBtn.textContent > 0 || keyBtn.textContent < 0
+					? keyBtn.textContent
+					: ""
+			);
+		}
+		try {
+			document.querySelector("#key button#selected").id = "";
+		} catch (e) {
+			console.error();
+		} finally {
+			if (!keyBtn.classList.contains("active")) {
+				deactivate();
+				activate(
+					[...cells]
+						.filter((elm) => elm.textContent == keyBtn.textContent)
+						.concat(keyBtn)
+				);
+				keyBtn.id = "selected";
+			} else {
+				deactivate();
+			}
+		}
+	});
 });
-// myArray = [1, 2, 3, 4, 5, 6, 8, 10];
-// function partition(array, isValid) {
-// 	return array.reduce(
-// 		([active, inactive], elem) => {
-// 			return isValid(elem)
-// 				? [[...active, elem], inactive]
-// 				: [active, [...inactive, elem]];
-// 		},
-// 		[[], []]
-// 	);
-// }
-// const x = partition(myArray, (e) => e < 5);
+
+// event by keydown
+document.addEventListener("keydown", (key) => {
+	let selectedCell = document.querySelector(".cell #selected");
+	if (/^\d+$/.test(key.key) && key.key > 0) {
+		deactivate();
+		activate([...cells].filter((elm) => elm.textContent == key.key));
+		if (selectedCell) {
+			assignValue(key.key);
+		}
+	}
+
+	if (key.keyCode == 8 || key.keyCode == 46) {
+		if (selectedCell) assignValue("");
+	}
+	if (key.keyCode == 27) {
+		deactivate();
+		try {
+			document.querySelectorAll("#selected").forEach((el) => {
+				el.id = "";
+			});
+		} catch (error) {}
+	}
+});
